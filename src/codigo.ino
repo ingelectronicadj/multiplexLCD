b9bb1ae3b4333c75cd5062520bf852c8dcc6cfb4
Diego Javier Mena Amado <glud@localhost.localdomain>
2017-06-19 16:16:46 -0500

    se carga codigo para jugar multiples LCD multiplexando el pin ENABLE

---
+++ src/codigo.ino
@@ -0,0 +1,49 @@
+/*
+ ***********************************************************************
+ *              LIBRERIAS y CONSTANTES
+ ***********************************************************************
+ */
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd1(12, 5, 7, 8, 9, 10); // LiquidCrystal(rs, enable, d4, d5, d6, d7)
+LiquidCrystal lcd2(12, 4, 7, 8, 9, 10);
+LiquidCrystal lcd3(12, 3, 7, 8, 9, 10);
+LiquidCrystal lcd4(12, 2, 7, 8, 9, 10);
+/*
+ ***********************************************************************
+ *              SETUP CONFIGURACIÓN INICIAL
+ ***********************************************************************
+ */
+void setup() {
+  // configura el numero de columnas y filas de cada lcd
+  //lcd1.begin(20, 4);
+  //lcd1.begin(8, 2);
+  lcd1.begin(16, 2);
+  lcd2.begin(16, 2);
+  lcd3.begin(16, 2);
+  lcd4.begin(16, 2);
+
+  // Imprime etiqueta en cada LCD
+  lcd1.print("Disp1");
+  lcd2.print("Disp2");
+  lcd3.print("Disp3");
+  lcd4.print("Disp4");
+}
+/*
+ ***********************************************************************
+ *              LOOP BUCLE PRINCIPAL
+ ***********************************************************************
+ */
+void loop() {
+  // configura el cursor de inicio para la escritura en cada LCD
+  lcd1.setCursor(0, 1); // columna 0 fila 2
+  lcd2.setCursor(0, 1);
+  lcd3.setCursor(0, 1);
+  lcd4.setCursor(0, 1);
+
+ // imprime el tiempo desde que arranca el programa
+  lcd1.print(millis()/1);
+  lcd2.print(millis()/10);
+  lcd3.print(millis()/100);
+  lcd4.print(millis()/1000);
+}
